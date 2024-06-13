3305654076b649ef538ef49f775e29a2922636fa
Damian Escobar <damianem611@gmail.com>
2024-06-13 14:07:26 +0200

    Cambios al fichero de java con el codigo para el ejercicio

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
  // Aquí tu código
+
+const lista = document.getElementById('lista');
+const botonAgregar = document.getElementById('agregar');
+
+botonAgregar.addEventListener('click', function() {
+    const nuevoElemento = prompt('Ingresa un nuevo elemento para la lista:');
+     const li = document.createElement('li');
+     li.textContent = nuevoElemento;
+     lista.appendChild(li);
+ } )
